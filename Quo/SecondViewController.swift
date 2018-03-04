a29c4007fe13ef647f3d9608655737057639e683
Federico Trimboli <federicotrimboli@gmail.com>
2018-03-04 19:48:30 -0300

    Crear proyecto de Xcode Closes #1

---
+++ Quo/SecondViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SecondViewController.swift
+//  Quo
+//
+//  Created by Federico Trimboli on 04/03/2018.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
