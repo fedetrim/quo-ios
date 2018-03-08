84f9c5b30ef9135340b594c1936e17a0c1386c90
Federico Trimboli <federicotrimboli@gmail.com>
2018-03-07 22:50:10 -0300

    Agregar celda de quote Closes #2 Closes #5

---
+++ Quo/QuotesTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  FirstViewController.swift
+//  Quo
+//
+//  Created by Federico Trimboli on 04/03/2018.
+//
+
+import UIKit
+
+class QuotesTableViewController: UITableViewController {
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+
+}
+
