42b8eda0449143d4fe59011e2811e73ad9c03c82
Federico Trimboli <federicotrimboli@gmail.com>
2018-03-14 20:52:18 -0300

    Agregar pantalla de Feed Closes #4

---
+++ Quo/ChronologicalQuotesViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ChronologicalQuotesViewController.swift
+//  Quo
+//
+//  Created by Federico Trimboli on 14/03/2018.
+//
+
+import UIKit
+
+final class ChronologicalQuotesViewController: UIViewController {
+    
+}
